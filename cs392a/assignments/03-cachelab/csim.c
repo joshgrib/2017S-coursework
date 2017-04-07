a3605d028d48422488fed6f84c80e9f17d13c733
Josh Gribbon <jgribbon@stevens.edu>
2017-04-06 22:32:35 -0400

    Finish hw

--- cs392a/assignments/03-cachelab/csim.c
+++ cs392a/assignments/03-cachelab/csim.c
@@ -5,160 +5,63 @@ Cache Simulator Lab
 I pledge my honor I have abided by the Stevens Honor System.
 ~Josh Gribbon
 */
-
-#include "cachelab.h"
-#include <stdio.h>
-#include <string.h>
-#include <getopt.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <strings.h>
+#include <string.h>
+#include <limits.h>
+#include "cachelab.h"
 
-//globals
+//input parameters
 static int SET_INDEX_BITS = 0;
 static int LINES_PER_SET = 0;
 static int BLOCK_BITS = 0;
 static char *TRACEFILE = NULL;
 static int VERBOSE_MODE = 0;
 
-//struct to pass result of simulation around
-struct csim_res {
-    int hits;
-    int misses;
-    int evictions;
-} cache_stats;
+//to hold 64 bit address
+typedef unsigned long long int addr_t;
 
-void recHit(){
-    cache_stats.hits++;
-    return;
-}
-void recMiss(){
-    cache_stats.misses++;
-    return;
-}
-void recEviction(){
-    cache_stats.evictions++;
-    return;
-}
+//cache structures
+typedef struct{
+    int valid;
+    addr_t tag;
+    int acc_num;
+} cache_line_t;
+typedef struct{
+    cache_line_t *lines;
+} cache_set_t;
+typedef struct{
+    cache_set_t *sets;
+} cache_t;
 
-void dataLoad(char *addr){
-    printf("dataLoad: %s\n", addr);
-    return;
-}
-void dataStore(char *addr){
-    printf("dataStore: %s\n", addr);
-    return;
-}
-void dataModify(char *addr){
-    printf("dataModify: %s\n", addr);
-    return;
-}
+//cache stats
+static int HITS = 0;
+static int MISSES = 0;
+static int EVICTIONS = 0;
+static int ACC_CNT = 0; //access count, for finding oldest access to evict
 
-char* simOp(char op, char *address){
-    //save current stats
-    int hs = cache_stats.hits;
-    int ms = cache_stats.misses;
-    int es = cache_stats.evictions;
-
-    //do operation
-    switch(op){
-        case 'L':
-            dataLoad(address);
-            break;
-        case 'S':
-            dataStore(address);
-            break;
-        case 'M':
-            dataModify(address);
-            break;
-    }
-
-    //find differences
-    int h_diff = cache_stats.hits - hs;
-    int m_diff = cache_stats.misses - ms;
-    int e_diff = cache_stats.evictions - es;
-
-    char *result = "";
-    for(int i=0; i<h_diff; i++){
-        strcat(result, " hit");
-    }
-    for(int i=0; i<m_diff; i++){
-        strcat(result, " miss");
-    }
-    for(int i=0; i<e_diff; i++){
-        strcat(result, " eviction");
-    }
-    return result;
-}
-
-void simCache(char *tracefile){
+void printUsage(char *argv[]){
     /*
-     *  Run cache simulation
-     *  Input: file containing vilgrind trace
-     *  Output: cache_sim_result struct
-     */
-
-    //initilize cache stats
-    cache_stats.hits = 4;
-    cache_stats.misses = 2;
-    cache_stats.evictions = 0;
-
-    //read file line by line
-    FILE *file;
-    file = fopen(tracefile, "r");
-    char line[64];
-    while(fgets(line, sizeof(line), file)){
-        //skip instruction loads
-        if(line[0] == 'I'){
-            continue;
-        }
-        //get operator
-        char op = line[1];
-        //figure out address length
-        int addr_len = 0;
-        while(line[addr_len+3] != ','){
-            addr_len++;
-        }
-        //get address
-        char address[addr_len];
-        for(int i=0; i<addr_len; i++){
-            address[i] = line[i+3];
-        }
-        address[addr_len] = '\0';
-        //get bytes
-        int bytes = line[addr_len+4];
-
-        //now we have the operation, address, and bytes
-        //lets see what happens when we try this
-        char *result = simOp(op, address);
-
-        if(VERBOSE_MODE){
-            printf("%c %s,%c %s\n", op, address, bytes, result);
-        }
-    }
-    fclose(file);
-
-    return;
+    Print out usage information for the program
+    */
+    printf( "Usage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0] );
+    printf(" -h:        Optional help flag that prints usage info\n");
+    printf(" -v:        Optional verbose flag that displays trace info\n");
+    printf(" -s <s>:    Number of set index bits (S = 2s is the number of sets)\n");
+    printf(" -E <E>:    Associativity (number of lines per set)\n");
+    printf(" -b <b>:    Number of block bits (B = 2b is the block size)\n");
+    printf(" -t <file>: Name of the valgrind trace to replay\n");
 }
 
-int main(int argc, char **argv) {
+void parseArgs(int argc, char **argv){
     /*
-     * Argument parsing
-     * [h] - print usage message
-     * [v] - set verbose mode, print trace info
-     *  s  - set index bit count
-     *  E  - set associativity (lines/set)
-     *  b  - set block bit count
-     *  t  - supply trace file
-     */
-    int opt = 0;
-    while ((opt=getopt(argc, argv, "hvs:E:b:t:")) != -1){
-        switch(opt){
-            case 'h':
-                printf("Usage: ./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
-                break;
-            case 'v':
-                VERBOSE_MODE = 1;
-                break;
+    Parse arguments and set program variables
+    */
+    char c;
+    while( (c = getopt(argc, argv, "hvs:E:b:t:")) != -1){
+        switch(c){
             case 's':
                 SET_INDEX_BITS = atoi(optarg);
                 break;
@@ -171,29 +74,191 @@ int main(int argc, char **argv) {
             case 't':
                 TRACEFILE = optarg;
                 break;
+            case 'v':
+                VERBOSE_MODE = 1;
+                break;
+            case 'h':
+                printUsage(argv);
+                exit( 0 );
             default:
-                printf("Usage: ./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
-                exit(1);
+                printUsage(argv);
+                exit( 1 );
         }
     }
+    return;
+}
 
-    //handle valid inputs with invalid values
-    if(SET_INDEX_BITS < 1){
-        printf("Error: Arg 's' - set index bits must be greater than 0\n");
-        exit(1);
+cache_t createCache(int set_index_bits, int lines_per_set){
+    /*
+    Set up memory for cache
+    */
+    cache_t cache;
+    //set size = 2^s
+    int SET_SIZE = ( 1 << SET_INDEX_BITS );
+    //allocate memory for sets
+    cache.sets = malloc(SET_SIZE * sizeof(cache_set_t));
+    //allocate memory for the lines in each set
+    for(int i=0; i<SET_SIZE; i++){
+        cache.sets[i].lines = malloc(LINES_PER_SET * sizeof(cache_line_t));
     }
-    if(LINES_PER_SET < 1){
-        printf("Error: Arg 'E' - associativity must be greater than 0\n");
-        exit(1);
-    }
-    if(BLOCK_BITS < 1){
-        printf("Error: Arg 'b' - block bits must be greater than 0\n");
-        exit(1);
-    }
+    return cache;
+}
 
-    //get results from running simulation
-    simCache(TRACEFILE);
-    //print summary
-    printSummary(cache_stats.hits, cache_stats.misses, cache_stats.evictions);
+int findTag(addr_t tag, cache_set_t set){
+    /*
+    look for the tag in the set
+    */
+    for(int e = 0; e < LINES_PER_SET; e++){
+        //only look in valid entries
+        if(set.lines[e].valid == 1){
+            //tag match = HIT!
+            if(set.lines[e].tag == tag){
+                HITS++;
+                set.lines[e].acc_num = ACC_CNT;
+                ACC_CNT++;
+                return e;
+            }
+        }
+    }
+    return -1;
+}
+
+int getFreeLine(cache_set_t set){
+    /*
+    Find an unused line in the set
+    */
+    for(int e = 0; e < LINES_PER_SET; e++){
+        if(set.lines[e].valid != 1){
+            return e;
+        }
+    }
+    return -1;
+}
+
+int getEvictLine(cache_set_t set){
+    /*
+    Find the line to evict in the set
+    */
+    int low = INT_MAX;
+    int target = 0;
+    for(int e = 0; e < LINES_PER_SET; e++){
+        if(set.lines[e].valid == 1){
+            if(set.lines[e].acc_num < low){
+                low = set.lines[e].acc_num;
+                target = e;
+            }
+        }
+    }
+    return target;
+}
+
+void accessCache(cache_t cache, addr_t address){
+    /*
+    Access the cache at the address
+    */
+    //get tag
+    addr_t tag = address >> (SET_INDEX_BITS + BLOCK_BITS);
+    //get set index
+    int tag_size = (64 - (SET_INDEX_BITS + BLOCK_BITS));
+    unsigned long long temp = address << (tag_size);
+    unsigned long long setID = temp >> (tag_size + BLOCK_BITS);
+    //get the set
+    cache_set_t set = cache.sets[setID];
+
+    //try to find the tag in the set
+    int match_line = findTag(tag, set);
+
+    //no HIT, let's add it
+    if(match_line == -1){
+        MISSES++;
+        int unused_line = getFreeLine(set);
+        //unused spot? put it there!
+        if(unused_line > -1){
+            set.lines[unused_line].valid = 1;
+            set.lines[unused_line].tag = tag;
+            set.lines[unused_line].acc_num = ACC_CNT;
+            ACC_CNT++;
+        }
+        //no empty spots? time to evict!
+        else if(unused_line < 0){
+            int evict_line = getEvictLine(set);
+            set.lines[evict_line].tag = tag;
+            set.lines[evict_line].acc_num = ACC_CNT;
+            ACC_CNT++;
+            EVICTIONS++;
+        }
+    }
+    return;
+}
+
+void cacheSim(cache_t cache){
+    /*
+    Simulate the cache using the input file
+    */
+    //operation, address, and size - to be read in from file
+    char op;
+    addr_t addr;
+    int size;
+
+    FILE *tfile = fopen(TRACEFILE, "r");
+
+    if(tfile!=NULL){
+        while(fscanf(tfile, " %c %llx,%d", &op, &addr, &size) == 3){
+            if(op == 'I') continue;
+
+            //save current stats
+            int hs = HITS;
+            int ms = MISSES;
+            int es = EVICTIONS;
+
+            //run operation
+            switch(op){
+                case 'L':
+                    accessCache(cache, addr);
+                    break;
+                case 'S':
+                    accessCache(cache, addr);
+                    break;
+                case 'M':
+                    accessCache(cache, addr);
+                    HITS++;//single access to load, guaranteed hit for saving
+                    break;
+            }
+
+            //find differences
+            int h_diff = HITS - hs;
+            int m_diff = MISSES - ms;
+            int e_diff = EVICTIONS - es;
+
+
+            //if the -v flag is set print out all debug information
+            if(VERBOSE_MODE == 1){
+                //build access summary
+                char *acc_sum = (char *) malloc(sizeof(char) * 200);
+                for(int i=0; i<h_diff; i++){
+                    strcat(acc_sum, "hit ");
+                }
+                for(int i=0; i<m_diff; i++){
+                    strcat(acc_sum, "miss ");
+                }
+                for(int i=0; i<e_diff; i++){
+                    strcat(acc_sum, "eviction ");
+                }
+                printf( "%c %llx,%d %s\n", op, addr, size, acc_sum);
+            }
+        }
+    }
+    return;
+}
+
+int main(int argc, char **argv) {
+    //parse arguments and set variables
+    parseArgs(argc, argv);
+    //allocate memory and create cache
+    cache_t cache = createCache(SET_INDEX_BITS, LINES_PER_SET);
+    //use the cache to simulate the trace
+    cacheSim(cache);
+    //print out values
+    printSummary(HITS, MISSES, EVICTIONS);
     return 0;
 }
